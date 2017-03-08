a3868073b874062c69b5cebbb97aa9f73b55e592
dustwise <jared.mohney@gmail.com>
2017-03-08 10:05:45 -0700

    skeleton files built

---
+++ js/components/gameboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ListContainer from './list-container';
+
+export default function Gameboard(props){
+    return (
+      <div>
+        <h1>Woodby the Hero</h1> 
+        <div className='board-list'>
+        </div>
+      </div>
+    );
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import Gameboard from './components/gameboard';
 
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<Gameboard />, document.getElementById('app'))
+);
